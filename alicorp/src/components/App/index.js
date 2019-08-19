a9d4d620062d4e9b71952d094042634c974ea375
kathmontalvo <kathmontalvo123@gmail.com>
2019-08-19 17:41:07 -0500

    routes

--- alicorp/src/components/App/index.js
+++ alicorp/src/components/App/index.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import firebase from '../../controller/firebase'
 import Login from '../Login'
 import Register from '../Register'
@@ -7,26 +7,24 @@ import Home from '../Products';
 import Purchase from '../Purchase'
 
 const App = () => {
-    const [firebaseInitialized, setFirebaseInitialized] = useState(false)
+  const [firebaseInitialized, setFirebaseInitialized] = useState(false)
 
-    useEffect(() => {
-        firebase.isInitialized().then(val => {
-            setFirebaseInitialized(val)
-        })
+  useEffect(() => {
+    firebase.isInitialized().then(val => {
+      setFirebaseInitialized(val)
     })
+  })
 
 
-    return firebaseInitialized !== false ? (
-        <Router>
-        <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/home" component={Home} />
-        </Switch>
-        </Router>
+  return firebaseInitialized !== false ? (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      {/* <Route exact path="/login" component={Login} /> */}
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/home" component={Home} />
+    </Switch>
 
-    ) : <div id="loader">Loading</div>
+  ) : <div id="loader">Loading</div>
 }
 
 export default App;
