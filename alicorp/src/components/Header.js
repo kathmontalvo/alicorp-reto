b577d2981e7868f5e0bbca658ce7e587f143a78a
PilarFernandez1304 <ccentepilar@gmail.com>
2019-08-20 18:57:12 -0500

    cambio de logo

--- alicorp/src/components/Header.js
+++ alicorp/src/components/Header.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
-import "../css/header.css"
+import "../css/header.css";
 
 const Header = () => {
-  
- 
-    
-  
-
   return (
     <header>
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <nav className="navbar navbar-expand-md navbar-dark color fixed-top">
         <button
           className="navbar-toggler navbar-togger-right"
           type="button"
@@ -21,32 +16,44 @@ const Header = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon" />
+          <span className="navbar-toggler-icon" />
         </button>
 
-        <a class="navbar-brand" href="#">
+        <a className="navbar-brand" href="#">
           <img
             data-testid="imgLogo"
-            style={{ maxWidth: "120px", maxHeigth: "120px" }}
+            style={{ maxWidth: "100px", maxHeigth: "100px" }}
             className="logo d-inline-block align-top"
-            src="https://i.postimg.cc/3JC9pBnv/1280px-Alicorp.pnghttps://i.postimg.cc/3JC9pBnv/1280px-Alicorp.png"
+            src="https://i.postimg.cc/qvqmHQNy/image.png"
             alt="logo"
           />
         </a>
-        <a class="navbar-brand" href="index.html" />
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <div id="barraNav" className=" d-sm-block d-md-none">
-            <ul className="d-xl-block">
-              <a className="containerNavList " href="#">
-                <li className="listNav ">Home</li>
-              </a>
-              <a className="containerNavList " href="#">
-                <li className="listNav">Features</li>
-              </a>
-              <a className="containerNavList " href="#">
-                <li className="listNav">Cerrar Sesion</li>{" "}
-              </a>
+          <div id="barraNav" className="d-sm-block d-md-none">
+            <ul className="nav d-xl-block">
+              <li className="nav-item containerNavList">
+                <a className="nav-link active listNav" href="#">
+                  Active
+                </a>
+              </li>
+              <li class="nav-item containerNavList">
+                <a class="nav-link listNav" href="#!">
+                  Link
+                </a>
+                
+              </li>
+              <li class="nav-item containerNavList">
+                <a class="nav-link active listNav" href="#">
+                  Active
+                </a>
+              </li>
+              
+              <li class="nav-item containerNavList">
+                <a class="nav-link listNav" href="#!">
+                  Cerrar Sesion
+                </a>
+              </li>
             </ul>
           </div>
         </div>
