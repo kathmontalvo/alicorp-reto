d63fd87abc8542de5d9ce7e0cd0b74fe5a1019c9
Daianatk <daiana.campos04@gmail.com>
2019-08-19 22:45:24 -0500

    subiendo

--- alicorp/src/components/Header.js
+++ alicorp/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Header = () => (
   <header>
     <Link to="/">
      <img
         data-testid="imgLogo"
-        className="center pb-2 pt-1 img-logo"
+        style={{maxWidth: '50%'}}
+        className="logo"
         src="https://i.postimg.cc/3JC9pBnv/1280px-Alicorp.pnghttps://i.postimg.cc/3JC9pBnv/1280px-Alicorp.png"
         alt="logo"
       ></img>
