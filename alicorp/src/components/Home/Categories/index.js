80e9a4cd90dfba17896493b3dfbaa147b3c59b51
Daianatk <daiana.campos04@gmail.com>
2019-08-20 21:46:25 -0500

    categorias

--- alicorp/src/components/Home/Categories/index.js
+++ alicorp/src/components/Home/Categories/index.js
@@ -9,32 +9,32 @@ const Section = () => {
       <div className="row">
         <div className="col-md-2">
           <div className="box">
-           <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/alicorp-900d0.appspot.com/o/iconos%2FACEITES.png?alt=media&token=ebb19d90-7285-4b3c-852a-8d929f899d84" alt=""></img>
+           <img className="d-block w-100" src="https://i.postimg.cc/k5XSYNyZ/ACEITES.png" alt=""></img>
           </div>
         </div>
         <div className="col-md-2">
           <div className="box">
-              <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/alicorp-900d0.appspot.com/o/iconos%2FCONSERVAS.png?alt=media&token=99d82a51-221f-41e4-9d63-6408810ee616" alt=""></img>
+              <img className="d-block w-100" src="https://i.postimg.cc/hjQfN2QC/CONSERVAS.png" alt=""></img>
           </div>
         </div>
         <div className="col-md-2">
           <div className="box">
-            <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/alicorp-900d0.appspot.com/o/iconos%2FPASTAS.png?alt=media&token=65ce17d7-8419-40ff-9e80-338f9d7de4f0" alt=""></img>
+            <img className="d-block w-100" src="https://i.postimg.cc/Fs47ngrZ/PASTAS.png" alt=""></img>
           </div>
         </div>
         <div class="col-md-2">
           <div class="box">
-              <img class="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/alicorp-900d0.appspot.com/o/iconos%2FDETER.png?alt=media&token=843464c2-6846-438d-836a-8535b0e8a9aa" alt=""></img>
+              <img class="d-block w-100" src="https://i.postimg.cc/rFjFnBYV/DETER.png" alt=""></img>
           </div>
         </div>
         <div className="col-md-2">
           <div className="box">
-            <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/alicorp-900d0.appspot.com/o/iconos%2FJABONES.png?alt=media&token=0a6ec257-15fe-4477-b90a-d4d59dd3f8a1" alt=""></img>
+            <img className="d-block w-100" src="https://i.postimg.cc/NG7gLht6/JABONES.png" alt=""></img>
           </div>
         </div>
         <div className="col-md-2">
           <div className="box">
-              <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/alicorp-900d0.appspot.com/o/iconos%2FCOMBOS.png?alt=media&token=53a8930f-356b-45e0-9fb9-eae1403cf1a0" alt=""></img>
+              <img className="d-block w-100" src="https://i.postimg.cc/8zsDyj64/COMBOS.png" alt=""></img>
           </div>
         </div>
       </div>
