fe8e9c83005ef2dd12fc8f1ff4689b3808230de7
kathmontalvo <kathmontalvo123@gmail.com>
2019-08-20 22:37:27 -0500

    category menu

--- alicorp/src/components/Home/Categories/index.js
+++ alicorp/src/components/Home/Categories/index.js
@@ -1,42 +1,19 @@
 import React from 'react';
+import Item from './Item';
 
-const Section = () => {
+const Category = () => {
 
   return (
     // <!--Carousel Wrapper-->
     <section className="boxes">
     <div className="container">
       <div className="row">
-        <div className="col-md-2">
-          <div className="box">
-           <img className="d-block w-100" src="https://i.postimg.cc/k5XSYNyZ/ACEITES.png" alt=""></img>
-          </div>
-        </div>
-        <div className="col-md-2">
-          <div className="box">
-              <img className="d-block w-100" src="https://i.postimg.cc/hjQfN2QC/CONSERVAS.png" alt=""></img>
-          </div>
-        </div>
-        <div className="col-md-2">
-          <div className="box">
-            <img className="d-block w-100" src="https://i.postimg.cc/Fs47ngrZ/PASTAS.png" alt=""></img>
-          </div>
-        </div>
-        <div class="col-md-2">
-          <div class="box">
-              <img class="d-block w-100" src="https://i.postimg.cc/rFjFnBYV/DETER.png" alt=""></img>
-          </div>
-        </div>
-        <div className="col-md-2">
-          <div className="box">
-            <img className="d-block w-100" src="https://i.postimg.cc/NG7gLht6/JABONES.png" alt=""></img>
-          </div>
-        </div>
-        <div className="col-md-2">
-          <div className="box">
-              <img className="d-block w-100" src="https://i.postimg.cc/8zsDyj64/COMBOS.png" alt=""></img>
-          </div>
-        </div>
+        <Item img="https://i.postimg.cc/k5XSYNyZ/ACEITES.png"/>
+        <Item img="https://i.postimg.cc/hjQfN2QC/CONSERVAS.png"/>
+        <Item img="https://i.postimg.cc/Fs47ngrZ/PASTAS.png"/>
+        <Item img="https://i.postimg.cc/rFjFnBYV/DETER.png"/>
+        <Item img="https://i.postimg.cc/NG7gLht6/JABONES.png"/>
+        <Item img="https://i.postimg.cc/8zsDyj64/COMBOS.png"/>
       </div>
     </div>
   </section>
@@ -44,4 +21,4 @@ const Section = () => {
   )
 }
 
-export default Section;
+export default Category;
